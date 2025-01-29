b8b5292fe9f738a1d261fc66d39d471bbe80f09c
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-01-29 17:22:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ r = st.number_input("Masukan Tinggi (cm): ",0)
 
 if st.button("Hitung Volume", type="primary"):
   loading = st.progress(0)
-  for i in range(100)
+  for i in range(100):
     time.sleep(0.01)
     loading.progress(i+1)
     
