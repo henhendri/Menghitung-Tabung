7146496db0cbc0325d2112da19fe0dd3f687fde7
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-01-31 17:42:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,17 @@ if st.button("Hitung Volume", type="primary"):
     
   v = math.pi*(r**2)*t
   st.success(f'Volume tabung adalah {v:.2f}')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
