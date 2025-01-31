49011af616032fff7321dc55bc52d1bc156e69e0
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-01-31 17:48:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 import math, time
+from st_pages import add_page_title, get_nav_from_toml
+
+st.set_page_config(layout="wide")
+
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
+
+nav = get_nav_from_toml(
+    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
+)
+
+st.logo("logo.png")
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
+
 
 st.title("Menghitung :blue[Volume Tabung] :rocket:")
 
