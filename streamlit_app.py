39d61366cc72803bb5668449de933c14802748ec
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-02-06 14:43:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,12 @@ import streamlit as st
 import math, time
 import random
 
-def toko_roti():
-  """Menghitung total belanjaan dan kembalian di toko roti."""
+st.title("Menghitung total belanjaan dan kembalian di toko roti.")
   harga_roti_tawar = 12000
   harga_kue_bolu = 25000
-  jumlah_roti_tawar = 3
-  jumlah_kue_bolu = 2
-  uang_bayar = 100000
+  jumlah_roti_tawar = st.number_input("Masukan Jumlah roti ",0)
+  jumlah_kue_bolu = st.number_input("Masukan Jumlah kue ",0)
+  uang_bayar = st.number_input("Masukan Jumlah bayar ",0)
 
   total_harga = (harga_roti_tawar * jumlah_roti_tawar) + (harga_kue_bolu * jumlah_kue_bolu)
   kembalian = uang_bayar - total_harga
