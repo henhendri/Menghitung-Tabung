497ebb2f542852e17ddcc82c4f7e1b6f68e072bd
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-02-06 15:17:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,7 @@
 import streamlit as st
 import math, time
-import random
 
-st.title("Menghitung total belanjaan dan kembalian di toko roti.")
-  harga_roti_tawar = 12000:
-  harga_kue_bolu = 25000:
-  jumlah_roti_tawar = st.number_input("Masukan Jumlah roti ",0)
-  jumlah_kue_bolu = st.number_input("Masukan Jumlah kue ",0)
-  uang_bayar = st.number_input("Masukan Jumlah bayar ",0)
-
-  total_harga = (harga_roti_tawar * jumlah_roti_tawar) + (harga_kue_bolu * jumlah_kue_bolu)
-  kembalian = uang_bayar - total_harga
-
-  print("Total belanjaan:", total_harga)
-  print("Kembalian:", kembalian)
-
-
-
+#menghitung volume tabung
 st.title("Menghitung :blue[Volume Tabung] :rocket:")
 
 r = st.number_input("Masukan Jari-Jari (cm): ",0)
@@ -30,6 +15,9 @@ if st.button("Hitung Volume", type="primary"):
     
   v = math.pi*(r**2)*t
   st.success(f'Volume tabung adalah {v:.2f}')
+
+
+
 
 col1, col2, col3 = st.columns(3)
 
